7023239d5ea6440f4db56afca2564f9b392bb885
Luca Monetti <73484162+Monetti-Luca@users.noreply.github.com>
2022-06-02 09:54:19 +0200

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+	entry: {
+		main: path.resolve(__dirname, "./src/index.js")
+	},
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "[name].bundle.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"]
+					}
+				}
+			}		
+		]
+	}
+}
